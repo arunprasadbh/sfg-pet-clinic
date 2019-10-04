2396bfc8325cba4ed9e63464102b069c7ef5effe
arunabhamidipati <arunprasad_bh@yahoo.com>
2019-10-04 16:00:29 +0100

    commit index controller and index.html

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/IndexController.java
@@ -4,5 +4,14 @@ package guru.springframework.sfgpetclinic.controllers;
  * Created by arunabhamidipati on 04/10/2019
  */
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class IndexController {
+
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String index(){
+        return "index";
+    }
 }
