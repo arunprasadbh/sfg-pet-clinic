632338a8c781fa516ad375225e62831110946d14
arunabhamidipati <arunprasad_bh@yahoo.com>
2019-10-10 04:28:15 +0100

    commit to owner_index_end

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnersController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnersController.java
@@ -1,8 +1,17 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /*
  * Created by arunabhamidipati on 05/10/2019
  */
-
+@Controller
 public class OwnersController {
+
+    @RequestMapping({"owners", "owners/index", "owners/index.html"})
+    public String ownersLit(){
+
+        return "owners/index";
+    }
 }
--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetsController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/VetsController.java
@@ -1,8 +1,17 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /*
  * Created by arunabhamidipati on 05/10/2019
  */
-
+@Controller
 public class VetsController {
+
+    @RequestMapping({"vets", "vets/index", "vets/index.html"})
+    public String vetsList(){
+
+        return "vets/index";
+    }
 }
