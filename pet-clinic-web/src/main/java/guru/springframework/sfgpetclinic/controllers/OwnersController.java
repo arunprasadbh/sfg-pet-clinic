b6d13ed2fe6938adf229f7da8f9cfae75b6fd2a1
arunabhamidipati <arunprasad_bh@yahoo.com>
2019-10-11 03:03:30 +0100

    Listing owners on Index Page complete. closes #14

--- pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnersController.java
+++ pet-clinic-web/src/main/java/guru/springframework/sfgpetclinic/controllers/OwnersController.java
@@ -1,17 +1,26 @@
 package guru.springframework.sfgpetclinic.controllers;
 
+import guru.springframework.sfgpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 /*
  * Created by arunabhamidipati on 05/10/2019
  */
 @Controller
+@RequestMapping("/owners")
 public class OwnersController {
+    private final OwnerService ownerService;
 
-    @RequestMapping({"owners", "owners/index", "owners/index.html"})
-    public String ownersLit(){
+    public OwnersController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
 
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String ownersLit(Model model){
+
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
