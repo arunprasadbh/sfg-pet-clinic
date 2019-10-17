f2933e6a382a06aa23c03c0427f26e3d5dfe2a58
arunabhamidipati <arunprasad_bh@yahoo.com>
2019-10-17 15:48:42 +0100

    Added JPA entities to Base Entity. Closes #18

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/BaseEntity.java
@@ -4,10 +4,14 @@ package guru.springframework.sfgpetclinic.model;
  * Created by arunabhamidipati on 03/10/2019
  */
 
+import javax.persistence.*;
 import java.io.Serializable;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     public Long id;
 
     public Long getId() {
