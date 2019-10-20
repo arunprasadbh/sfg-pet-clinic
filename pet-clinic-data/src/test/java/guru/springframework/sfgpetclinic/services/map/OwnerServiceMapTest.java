09be4c2dd193865c9fd42be75324f0815a3d0691
arunabhamidipati <arunprasad_bh@yahoo.com>
2019-10-20 09:11:04 +0100

    deleted  junit-platform-surefire-provider in pom

---
+++ pet-clinic-data/src/test/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMapTest.java
@@ -0,0 +1,42 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+/*
+ * Created by arunabhamidipati on 20/10/2019
+ */
+
+class OwnerServiceMapTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void findAll() {
+    }
+
+    @Test
+    void deleteById() {
+    }
+
+    @Test
+    void delete() {
+    }
+
+    @Test
+    void save() {
+    }
+
+    @Test
+    void findById() {
+    }
+
+    @Test
+    void findByLastName() {
+    }
+}
